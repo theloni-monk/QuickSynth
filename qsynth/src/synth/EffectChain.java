d45a15e5c81b1cd0bc33fcb4409fe6d6e63872da
Theo <13920881+theloni-monk@users.noreply.github.com>
2020-05-16 11:13:41 -0700

    fix effect chain bug

--- qsynth/src/synth/EffectChain.java
+++ qsynth/src/synth/EffectChain.java
@@ -5,11 +5,11 @@ public class EffectChain {
     protected ArrayList<AbstractAffector> effects;
 
     public EffectChain(){
-
+        this.effects = new ArrayList<AbstractAffector>();
     }
 
     public double[] process(double[] input){
-        double[] output = new double[input.length]; // it might make more sense to have it be longer than input for things like reverb. hmmm...
+        double[] output = input; //needs deep copy? // it might make more sense to have it be longer than input for things like reverb. hmmm...
         for(AbstractAffector effect: this.effects){
             output = effect.process(output);
         }
